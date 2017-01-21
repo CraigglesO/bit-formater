016d444a0c2a586d90b09cd3ca741c73468222fb
oconnorct1 <oconnorct1@gmail.com>
2017-01-21 04:36:41 -0500

    some changes

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,11 @@
 import test = require('blue-tape');
 
-import bitFormater = require('bit-formater');
+// import bitFormater = require('bit-formater');
+import bitFormater from '../bit-formater';
+
+
+test('See the output from bitFormater', (t) => {
+  let x = bitFormater(234567543, 2);
+  t.equal(x, '234.568 MB');
+  t.end();
+})
